7bd486627a7be7981da44b10c9c990cc8ee759e1
Trần Phúc <phuckhung411@gmail.com>
2020-07-11 17:21:17 +0700

    chang

--- constant.js
+++ constant.js
@@ -1,3 +1,3 @@
-// let base_url = "http://52.43.108.161:8181/";
-let base_url = "http://127.0.0.1:8080/";
+let base_url = "http://52.43.108.161:8181/";
+// let base_url = "http://127.0.0.1:8080/";
 
